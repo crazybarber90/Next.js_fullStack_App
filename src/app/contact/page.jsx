59873aaa7931c3978d7d5ad77a2f7a48cef058ca
koladosoma <koladosoma23@gmail.com>
2024-04-25 11:37:42 +0200

    hydration common issue, explanation, and use case

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,27 @@
-// "use client";
+'use client'
 import Image from 'next/image'
 import styles from './contact.module.css'
 
-export const metadata = {
-  title: 'Contact Page',
-  description: 'Contact description',
-}
+// export const metadata = {
+//   title: 'Contact Page',
+//   description: 'Contact description',
+// }
 
+//====================================================== HYDRATION
+
+//HYDRATION POTENTIAL ISSUE's
+//this is a comm potential issue with hydration i
+
+//this is the most common error that can occur in the next project regarding hydration, because one value of the variable appears on the server, because the function is certainly triggered first on the server
+
+const a = Math.random()
+console.log('a random numberas ', a)
+// ON CLIENT a random numberas  0.07542550531041625
+// ON SERVER a random numberas  0.7126854815407451
+
+// regardless of whether the component is client-side, and then only on the client-side and various values ​​of the same variable lead to a conflict
+
+//====================================================== HYDRATION
 const ContactPage = () => {
   return (
     <div className={styles.container}>
